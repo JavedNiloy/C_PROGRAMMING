29a78d30574cedb3dd2c4676730069aa365b2c54
JavedNiloy <tasnimjaved.niloy@gmail.com>
2024-07-11 21:39:25 +0600

    11_july_2024

---
+++ c_programming/new.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, sum;
+    a = 5;
+    b = 6;
+    sum = a + b;
+    printf("the sum is: %d", sum);
+    scanf("press any key to continue: ");
+    return 0;
+}
---
+++ m.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	a = 5;
+	b = 7;
+	c = a + b;
+	printf("c = %d", c);
+	scanf("press any key to continue: ");
+}
